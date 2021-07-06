466fc9ae94adf44738f442b752ea5d19c5c63937
daxxx <daxxx@DESKTOP-7H7HJEP>
2021-07-06 17:29:37 +0900

    JaeHyeon : E.java create

---
+++ MainProject/src/test/java/user/E.java
@@ -0,0 +1,14 @@
+package user;
+
+public class E {
+
+	public E() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Fewfa");
+	}
+
+}
